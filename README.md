64c313b51cb2575498e4e202796d7031505df256
Abdulrahman <sonayen.a@gmail.com>
2018-05-04 16:27:16 +0300

    Make code more readbale, add noBorder/material, add prop-types

--- README.md
+++ README.md
@@ -18,32 +18,33 @@ type button = {
   backgroundColor?: string,         // default = white
   bounce?: boolean,                 // default = false
   disabled?: boolean,               // default = false
-  disabledBackgroundColor: string,  // default = gray
-  disabledForegroundColor: string,  // default = white
-  errorBackgroundColor: string,     // default = red
+  disabledBackgroundColor?: string, // default = gray
+  disabledForegroundColor?: string, // default = white
+  errorBackgroundColor?: string,    // default = red
   errorForegroundColor?: string,    // default = white
-  errorIcon: string,
-  errorLabel: string,
+  errorIcon?: string,
+  errorLabel?: string,
   expandOnFinish?: boolean,         // default = false
   foregroundColor?: string,         // default = blue
-  icon?: string,                    // default = icons names from FontAwesome
+  icon?: string,                    // default = icons names from iconSet
   iconSet? any,                     // default = FontAwesome
   iconSize?: number,                // default = 17
   iconStyle?: Object,
-  initialState?: string,            // default = null, can have values ['success', 'error', 'loading']
-  label: string,
+  initialState?: 'success' | 'error' | 'loading',
+  label?: string,
   labelStyle?: Object,              // default = defaultLabelStyle
+  material?: boolean,               // use MaterialIcons instead of FontAwesome
   maxWidth?: number,                // default = 240
   minWidth?: number,                // default = 40
-  width?: number,                   // default = 240, overwrites maxWidth and minWidth, use for fixed length
+  noBorder?: boolean,               // default = false
   noFill?: boolean,                 // default = false
   noRadius?: boolean,               // default = false
-  onError?: Function,               // default = () => null
-  onLoad?: Function,                // default = () => null
-  onPress?: Function,               // default = () => null
-  onReset?: Function,               // default = () => null
-  onSecondaryPress?: Function,      // default = () => null
-  onSuccess?: Function,             // default = () => null
+  onError?: Function,
+  onLoad?: Function,
+  onPress?: Function,
+  onReset?: Function,
+  onSecondaryPress?: Function,
+  onSuccess?: Function,
   renderErrorIcon?: any,            // default = <FontAwesome />
   renderIndicator?: any,            // default = <ActivityIndicator />
   renderLabel?: any,                // default = <Text />
@@ -54,14 +55,15 @@ type button = {
   static?: boolean,                 // default = false
   style?: Object,                   // default = defaultStyle
   successBackgroundColor?: string,  // default = green
-  successForegroundColor?: string   // default = white
-  successIcon: string,
-  successLabel: string,
+  successForegroundColor?: string,  // default = white
+  successIcon?: string,
+  successLabel?: string,
+  width?: number,                   // overwrites maxWidth and minWidth, use for fixed length
 };
 
 const defaultStyle = {
   alignItems: 'center',
-  borderRadius: 40 / 2,
+  borderRadius: 20,
   borderWidth: 1,
   height: 40,
   justifyContent: 'center',
@@ -74,205 +76,12 @@ const defaultLabelStyle = {
 };
 
 // methods
-button.error();   // Animates button to error state
-button.load();    // Animates button to loading state
-button.reset();   // Animates button to initial/default state
-button.success(); // Animates button to success state
+button.success(); // Animate button to success state
+button.error();   // Animate button to error state
+button.load();    // Animate button to loading state
+button.reset();   // Animate button to initial/default state
 ```
 
 ## Examples
 
-```javascript
-import React, { PureComponent } from 'react';
-import { Platform, ScrollView, StatusBar, Text, View } from 'react-native';
-
-import Btn from 'react-native-micro-animated-button';
-
-StatusBar.setHidden(true, 'fade');
-
-const colors =
-  Platform.OS === 'ios'
-    ? {
-        blue: '#007aff',
-        gray: '#d8d8d8',
-        green: '#4cd964',
-        red: '#ff3b30',
-        white: '#ffffff'
-      }
-    : {
-        blue: '#4285f4',
-        gray: '#d8d8d8',
-        green: '#0f9d58',
-        red: '#db4437',
-        white: '#ffffff'
-      };
-
-const Example1 = () => (
-  <View style={styles.center}>
-    <Btn
-      foregroundColor={colors.green}
-      label="Submit"
-      onPress={() => this.b1.success()}
-      ref={ref => (this.b1 = ref)}
-      successIcon="check"
-    />
-
-    <Btn
-      foregroundColor={colors.blue}
-      label="Retweet"
-      onPress={() => this.b2.success()}
-      ref={ref => (this.b2 = ref)}
-      successIcon="retweet"
-    />
-
-    <Btn
-      foregroundColor={colors.red}
-      label="Favorite"
-      onPress={() => this.b3.success()}
-      ref={ref => (this.b3 = ref)}
-      successIcon="heart"
-    />
-  </View>
-);
-
-const Example2 = () => (
-  <View style={styles.center}>
-    <Btn
-      errorBackgroundColor={colors.red}
-      errorIcon="thumbs-down"
-      expandOnFinish
-      foregroundColor={colors.blue}
-      label="Am I even?"
-      onPress={() =>
-        new Date().getSeconds() % 2 === 0 ? this.b4.success() : this.b4.error()
-      }
-      ref={ref => (this.b4 = ref)}
-      successBackgroundColor={colors.green}
-      successIcon="thumbs-up"
-      width={240}
-    />
-
-    <Btn
-      errorBackgroundColor={colors.red}
-      errorIcon="thumbs-down"
-      expandOnFinish
-      foregroundColor={colors.blue}
-      label="Am I even?"
-      onPress={() =>
-        new Date().getSeconds() % 2 === 0 ? this.b5.success() : this.b5.error()
-      }
-      ref={ref => (this.b5 = ref)}
-      successBackgroundColor={colors.green}
-      successIcon="thumbs-up"
-      width={240}
-    />
-  </View>
-);
-
-const Example3 = () => (
-  <View style={styles.center}>
-    <Btn
-      backgroundColor={colors.blue}
-      errorBackgroundColor={colors.red}
-      errorForegroundColor={colors.white}
-      errorIcon="warning"
-      foregroundColor={colors.white}
-      label="Simulate an error"
-      onPress={() => this.b6.error()}
-      ref={ref => (this.b6 = ref)}
-      shakeOnError
-      width={240}
-    />
-
-    <Btn
-      backgroundColor={colors.blue}
-      foregroundColor={colors.white}
-      label="Smile at me"
-      onPress={() => this.b7.success()}
-      ref={ref => (this.b7 = ref)}
-      scaleOnSuccess
-      successBackgroundColor={colors.green}
-      successForegroundColor={colors.white}
-      successIcon="smile-o"
-      width={240}
-    />
-  </View>
-);
-
-class Example4 extends PureComponent {
-  state = { disabled: true };
-
-  onPress = () => this.setState({ disabled: !this.state.disabled });
-
-  render() {
-    return (
-      <View style={styles.center}>
-        <Btn
-          backgroundColor={colors.blue}
-          foregroundColor={colors.white}
-          label="Static Button"
-          noRadius
-          onPress={this.onPress}
-          static
-        />
-
-        <Btn
-          disabled={this.state.disabled}
-          label="Disabled Button"
-          noRadius
-          static
-        />
-      </View>
-    );
-  }
-}
-
-class Example5 extends PureComponent {
-  state = { clicked: false };
-
-  onPress = () => this.setState({ clicked: true }, () => this.b8.success());
-
-  onSecondaryPress = () =>
-    this.setState({ clicked: false }, () => this.b8.reset());
-
-  render() {
-    return (
-      <View style={styles.row}>
-        <Btn
-          foregroundColor={colors.blue}
-          icon="cloud-download"
-          noFill
-          onPress={this.onPress}
-          onSecondaryPress={this.onSecondaryPress}
-          ref={ref => (this.b8 = ref)}
-          successBackgroundColor={colors.blue}
-          successIcon="remove"
-        />
-
-        {this.state.clicked && (
-          <Text style={styles.rightText}>I just got downloaded</Text>
-        )}
-      </View>
-    );
-  }
-}
-
-const Examples = () => (
-  <ScrollView contentContainerStyle={styles.full}>
-    <Example1 />
-    <Example2 />
-    <Example3 />
-    <Example4 />
-    <Example5 />
-  </ScrollView>
-);
-
-const styles = {
-  center: { alignItems: 'center' },
-  full: { flex: 1 },
-  rightText: { color: colors.blue, marginLeft: 10 },
-  row: { alignItems: 'center', flexDirection: 'row', justifyContent: 'center' }
-};
-
-export default Examples;
-```
+Available [here](https://raw.githubusercontent.com/sonaye/react-native-micro-animated-button/master/examples.js).
--- button.js
+++ button.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import {
   ActivityIndicator,
   Animated,
@@ -10,6 +11,9 @@ import {
 import TouchableBounce from 'react-native/Libraries/Components/Touchable/TouchableBounce';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import PropTypes from 'prop-types';
 
 const colors =
   Platform.OS === 'ios'
@@ -28,14 +32,101 @@ const colors =
         white: '#ffffff'
       };
 
-export default class extends Component {
+export default class MicroAnimatedButton extends Component {
+  static propTypes = {
+    activeOpacity: PropTypes.number,
+    backgroundColor: PropTypes.string,
+    bounce: PropTypes.bool,
+    disabled: PropTypes.bool,
+    disabledBackgroundColor: PropTypes.string,
+    disabledForegroundColor: PropTypes.string,
+    errorBackgroundColor: PropTypes.string,
+    errorForegroundColor: PropTypes.string,
+    errorIcon: PropTypes.string,
+    errorLabel: PropTypes.string,
+    expandOnFinish: PropTypes.bool,
+    foregroundColor: PropTypes.string,
+    icon: PropTypes.string,
+    iconSet: PropTypes.any,
+    iconSize: PropTypes.number,
+    iconStyle: PropTypes.object,
+    initialState: PropTypes.oneOf(['success', 'error', 'loading']),
+    label: PropTypes.string,
+    labelStyle: PropTypes.object,
+    material: PropTypes.bool,
+    maxWidth: PropTypes.number,
+    minWidth: PropTypes.number,
+    noBorder: PropTypes.bool,
+    noFill: PropTypes.bool,
+    noRadius: PropTypes.bool,
+    onError: PropTypes.func,
+    onLoad: PropTypes.func,
+    onPress: PropTypes.func,
+    onReset: PropTypes.func,
+    onSecondaryPress: PropTypes.func,
+    onSuccess: PropTypes.func,
+    renderErrorIcon: PropTypes.any,
+    renderIndicator: PropTypes.any,
+    renderLabel: PropTypes.any,
+    renderSuccessIcon: PropTypes.any,
+    scaleFactor: PropTypes.number,
+    scaleOnSuccess: PropTypes.bool,
+    shakeOnError: PropTypes.bool,
+    static: PropTypes.bool,
+    style: PropTypes.object,
+    successBackgroundColor: PropTypes.string,
+    successForegroundColor: PropTypes.string,
+    successIcon: PropTypes.string,
+    successLabel: PropTypes.string,
+    width: PropTypes.number
+  };
+
   static defaultProps = {
+    activeOpacity: undefined,
+    backgroundColor: undefined,
+    bounce: false,
+    disabled: false,
+    disabledBackgroundColor: undefined,
+    disabledForegroundColor: undefined,
+    errorBackgroundColor: undefined,
+    errorForegroundColor: undefined,
     errorIcon: 'warning',
+    errorLabel: undefined,
+    expandOnFinish: false,
+    foregroundColor: undefined,
+    icon: undefined,
+    iconSet: undefined,
     iconSize: 17,
+    iconStyle: {},
+    initialState: undefined,
+    label: undefined,
+    labelStyle: {},
+    material: false,
     maxWidth: 240,
     minWidth: 40,
+    noBorder: false,
+    noFill: false,
+    noRadius: false,
+    onError: undefined,
+    onLoad: undefined,
+    onPress: undefined,
+    onReset: undefined,
+    onSecondaryPress: undefined,
+    onSuccess: undefined,
+    renderErrorIcon: undefined,
+    renderIndicator: undefined,
+    renderLabel: undefined,
+    renderSuccessIcon: undefined,
     scaleFactor: 1.1,
-    successIcon: 'check'
+    scaleOnSuccess: false,
+    shakeOnError: false,
+    static: false,
+    style: {},
+    successBackgroundColor: undefined,
+    successForegroundColor: undefined,
+    successIcon: 'check',
+    successLabel: undefined,
+    width: undefined
   };
 
   initialStep = this.props.disabled
@@ -51,75 +142,17 @@ export default class extends Component {
     error: this.props.initialState === 'error'
   };
 
-  animated = new Animated.Value(this.initialStep);
-
-  micro = new Animated.Value(0);
-
   componentWillReceiveProps(nextProps) {
+    // auto-animate when disable/enable
     const { disabled } = this.props;
-
     if (nextProps.disabled !== disabled) this.disable(disabled);
   }
 
-  successBackgroundColor = this.animated.interpolate({
-    inputRange: [0, 1, 2, 3],
-    outputRange: [
-      this.props.disabledBackgroundColor || colors.white,
-      this.props.backgroundColor || colors.white,
-      this.props.backgroundColor || colors.white,
-      this.props.noFill
-        ? this.props.backgroundColor || colors.white
-        : this.props.successBackgroundColor ||
-          this.props.foregroundColor ||
-          colors.green
-    ]
-  });
+  // animation engines
+  animated = new Animated.Value(this.initialStep);
+  micro = new Animated.Value(0);
 
-  errorBackgroundColor = this.animated.interpolate({
-    inputRange: [0, 1, 2, 3],
-    outputRange: [
-      this.props.disabledBackgroundColor || colors.white,
-      this.props.backgroundColor || colors.white,
-      this.props.backgroundColor || colors.white,
-      this.props.noFill
-        ? this.props.backgroundColor || colors.white
-        : this.props.errorBackgroundColor ||
-          this.props.foregroundColor ||
-          colors.red
-    ]
-  });
-
-  successForegroundColor = this.animated.interpolate({
-    inputRange: [0, 1, 2, 3],
-    outputRange: [
-      this.props.disabledForegroundColor || colors.gray,
-      this.props.foregroundColor || colors.blue,
-      this.props.foregroundColor || colors.blue,
-      this.props.noFill
-        ? this.props.successForegroundColor ||
-          this.props.foregroundColor ||
-          colors.blue
-        : this.props.successForegroundColor ||
-          this.props.backgroundColor ||
-          colors.white
-    ]
-  });
-
-  errorForegroundColor = this.animated.interpolate({
-    inputRange: [0, 1, 2, 3],
-    outputRange: [
-      this.props.disabledForegroundColor || colors.gray,
-      this.props.foregroundColor || colors.blue,
-      this.props.foregroundColor || colors.blue,
-      this.props.noFill
-        ? this.props.errorForegroundColor ||
-          this.props.foregroundColor ||
-          colors.blue
-        : this.props.errorForegroundColor ||
-          this.props.backgroundColor ||
-          colors.white
-    ]
-  });
+  // animations
 
   width = this.props.width ||
   this.animated.interpolate({
@@ -132,26 +165,95 @@ export default class extends Component {
     ]
   });
 
-  shake = this.micro.interpolate({
-    inputRange: [0, 1, 2],
-    outputRange: [0, 10, -10]
+  // animation colors
+
+  getBackgroundColor = (success = true) => {
+    const {
+      backgroundColor,
+      disabledBackgroundColor,
+      errorBackgroundColor,
+      foregroundColor,
+      noFill,
+      successBackgroundColor
+    } = this.props;
+
+    const defaultBackgroundColor = colors.white;
+    const defaultErrorColor = colors.red;
+    const defaultSuccessColor = colors.green;
+
+    return [
+      disabledBackgroundColor || defaultBackgroundColor,
+      backgroundColor || defaultBackgroundColor,
+      backgroundColor || defaultBackgroundColor,
+      noFill
+        ? backgroundColor || defaultBackgroundColor
+        : success
+          ? successBackgroundColor || foregroundColor || defaultSuccessColor
+          : errorBackgroundColor || foregroundColor || defaultErrorColor
+    ];
+  };
+
+  getForegroundColor = (success = true) => {
+    const {
+      backgroundColor,
+      disabledForegroundColor,
+      errorForegroundColor,
+      foregroundColor,
+      noFill,
+      successForegroundColor
+    } = this.props;
+
+    const defaultBackgroundColor = colors.white;
+    const defaultDisabledForegroundColor = colors.gray;
+    const defaultForegroundColor = colors.blue;
+
+    return [
+      disabledForegroundColor || defaultDisabledForegroundColor,
+      foregroundColor || defaultForegroundColor,
+      foregroundColor || defaultForegroundColor,
+      noFill
+        ? (success ? successForegroundColor : errorForegroundColor) ||
+          foregroundColor ||
+          defaultForegroundColor
+        : (success ? successForegroundColor : errorForegroundColor) ||
+          backgroundColor ||
+          defaultBackgroundColor
+    ];
+  };
+
+  successBackgroundColor = this.animated.interpolate({
+    inputRange: [0, 1, 2, 3],
+    outputRange: this.getBackgroundColor()
   });
+
+  errorBackgroundColor = this.animated.interpolate({
+    inputRange: [0, 1, 2, 3],
+    outputRange: this.getBackgroundColor(false)
+  });
+
+  successForegroundColor = this.animated.interpolate({
+    inputRange: [0, 1, 2, 3],
+    outputRange: this.getForegroundColor()
+  });
+
+  errorForegroundColor = this.animated.interpolate({
+    inputRange: [0, 1, 2, 3],
+    outputRange: this.getForegroundColor(false)
+  });
+
+  // micro animations
 
   scale = this.micro.interpolate({
     inputRange: [0, 1],
     outputRange: [1, this.props.scaleFactor]
   });
 
-  animate = (step, { cb, error = false, timing = false, micro } = {}) => {
-    this.setState({ step, error }, () =>
-      (timing ? Animated.timing : Animated.spring)(this.animated, {
-        toValue: step,
-        ...(timing ? { duration: 250 } : {})
-      }).start(({ finished }) => finished && cb && cb())
-    );
+  shake = this.micro.interpolate({
+    inputRange: [0, 1, 2],
+    outputRange: [0, 10, -10]
+  });
 
-    if (micro) micro();
-  };
+  // micro animation players
 
   microScale = () =>
     this.props.scaleOnSuccess &&
@@ -168,16 +270,31 @@ export default class extends Component {
       Animated.timing(this.micro, { toValue: 0, duration: 40 })
     ]).start();
 
-  disable = disabled => this.animate(disabled ? 1 : 0, { timing: true });
-
-  reset = () => this.animate(1, { cb: this.props.onReset });
+  // events
 
   onPress = () =>
     this.props.static
       ? this.props.onPress && this.props.onPress()
       : this.animate(2, { cb: this.props.onPress });
 
-  load = () => this.animate(2, { cb: this.props.onLoad });
+  // private methods
+
+  animate = (step, { cb, error = false, timing = false, micro } = {}) => {
+    this.setState({ step, error }, () => {
+      const type = timing ? Animated.timing : Animated.spring;
+
+      type(this.animated, {
+        toValue: step,
+        ...(timing ? { duration: 250 } : {})
+      }).start(({ finished }) => finished && cb && cb());
+    });
+
+    if (micro) micro();
+  };
+
+  disable = disabled => this.animate(disabled ? 1 : 0, { timing: true });
+
+  // public methods
 
   success = () =>
     this.animate(3, { cb: this.props.onSuccess, micro: this.microScale });
@@ -189,12 +306,18 @@ export default class extends Component {
       micro: this.microShake
     });
 
-  animatedIcon = Animated.createAnimatedComponent(
-    this.props.iconSet || FontAwesome
-  );
+  load = () => this.animate(2, { cb: this.props.onLoad });
+
+  reset = () => this.animate(1, { cb: this.props.onReset });
+
+  // render helpers
 
   renderLabel = (label, style) => (
     <Animated.Text style={style}>{label}</Animated.Text>
+  );
+
+  animatedIcon = Animated.createAnimatedComponent(
+    this.props.iconSet || this.props.material ? MaterialIcons : FontAwesome
   );
 
   renderIcon = (icon, color) => (
@@ -205,6 +328,10 @@ export default class extends Component {
     />
   );
 
+  renderIndicator = () => (
+    <ActivityIndicator color={this.props.foregroundColor || colors.blue} />
+  );
+
   render() {
     const {
       activeOpacity,
@@ -212,10 +339,9 @@ export default class extends Component {
       disabled,
       errorIcon,
       errorLabel,
-      foregroundColor,
       icon,
       label,
-      labelStyle,
+      noBorder,
       noRadius,
       onSecondaryPress,
       renderErrorIcon,
@@ -240,6 +366,8 @@ export default class extends Component {
       width
     } = this;
 
+    // colors
+
     const animatedBackgroundColor = error
       ? errorBackgroundColor
       : successBackgroundColor;
@@ -247,6 +375,8 @@ export default class extends Component {
     const animatedForegroundColor = error
       ? errorForegroundColor
       : successForegroundColor;
+
+    // styles
 
     const buttonStyle = [
       {
@@ -256,54 +386,66 @@ export default class extends Component {
         width
       },
       styles.button,
+      // if noBorder or noRadius
+      noBorder && { borderWidth: 0 },
       noRadius && { borderRadius: 0 },
       style
     ];
 
-    const newLabelStyle = [
+    const labelStyle = [
       { color: animatedForegroundColor },
       styles.label,
-      labelStyle
+      this.props.labelStyle
     ];
 
     const button = (
       <Animated.View style={buttonStyle}>
         {(step === 0 || step === 1) && (
           <View>
-            {renderLabel ||
+            {renderLabel || // render renderLabel() (custom) > icon > label
               (icon
                 ? this.renderIcon(icon, animatedForegroundColor)
-                : this.renderLabel(label, newLabelStyle))}
+                : this.renderLabel(label, labelStyle))}
           </View>
         )}
 
-        {step === 2 &&
-          (renderIndicator || (
-            <ActivityIndicator color={foregroundColor || colors.blue} />
-          ))}
+        {/* render renderIndicator() (custom) > default indicator */}
+        {step === 2 && (renderIndicator || this.renderIndicator())}
 
         {step === 3 &&
           (error
-            ? errorLabel
-              ? this.renderLabel(errorLabel, newLabelStyle)
+            ? // if error: render errorLabel > renderErrorIcon() (custom) > errorIcon
+              errorLabel
+              ? this.renderLabel(errorLabel, labelStyle)
               : renderErrorIcon ||
                 this.renderIcon(errorIcon, errorForegroundColor)
-            : successLabel
-              ? this.renderLabel(successLabel, newLabelStyle)
+            : // if success: render successLabel > renderSuccessIcon() (custom) > successIcon
+              successLabel
+              ? this.renderLabel(successLabel, labelStyle)
               : renderSuccessIcon ||
                 this.renderIcon(successIcon, successForegroundColor))}
       </Animated.View>
     );
 
+    // touchable props
     const props = {
       children: button,
+
+      // disable when:
+      // - loading state
+      // - success/error state without a secondary onPress
+      // - actually disabled (forced)
       disabled: step === 2 || (step === 3 && !onSecondaryPress) || disabled,
+
+      // onPress in all cases but when success/error state with a secondary onPress
       onPress: (step === 3 && onSecondaryPress) || onPress,
+
+      // pass active opacity prop if TouchableOpacity
       ...(!bounce ? { activeOpacity } : {})
     };
 
+    // render touchable
     if (bounce) return <TouchableBounce {...props} />;
-
     return <TouchableOpacity {...props} />;
   }
 }
@@ -311,7 +453,7 @@ export default class extends Component {
 const styles = {
   button: {
     alignItems: 'center',
-    borderRadius: 40 / 2,
+    borderRadius: 20,
     borderWidth: 1,
     height: 40,
     justifyContent: 'center',
